3b039ba2d580b291ee759ed78e4975d279be1bba
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-02-24 23:17:28 +0100

    Worksapce stuff + tests
    Property-based testing of parsing workspace names ftw.

--- src/I3/Workspaces.hs
+++ src/I3/Workspaces.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module I3.Workspaces
-  ( getWorkspaces
-  ) where
+module I3.Workspaces where
 
+import Control.Arrow ((>>>))
 import Data.Aeson (FromJSON, decode)
+import Data.ByteString.Lazy.UTF8 (fromString)
+import Data.Char
+import Data.Foldable
 import GHC.Generics
 import I3.IPC
 import I3.Internal
+import Text.ParserCombinators.ReadP
+import Text.Read (readMaybe)
 
 data Geometry = Geometry
   { x :: Int
@@ -37,3 +43,44 @@ getWorkspaces i3 = do
   case decode payload of
     Nothing -> fail "Invalid workspace response"
     Just res -> pure res
+
+renumberWorkspaces :: [String] -> [String]
+renumberWorkspaces = zipWith newName (map show [1 :: Int ..])
+  where newName i old =
+          let (_, label) = workspaceNumber old
+          in if null label then i else i <> ": " <> label
+
+renameWorkspace :: I3 -> String -> String -> IO ()
+renameWorkspace i3 old new = do
+  let sock = i3CmdSocket i3
+      cmd = "rename workspace \"" <> fromString old <> "\" to \"" <> fromString new <> "\""
+  res <- invoke sock (Request RunCommand cmd)
+  print res
+
+assignWorkspaceNumbers :: I3 -> IO ()
+assignWorkspaceNumbers i3 = do
+  workspaces <- map name <$> getWorkspaces i3
+  let renames = filter (uncurry (/=)) $ zip workspaces (renumberWorkspaces workspaces)
+  traverse_ (uncurry $ renameWorkspace i3) renames
+
+parse :: Show a => ReadP a -> String -> (Maybe a, String)
+parse parser input = case readP_to_S parser input of
+  [] -> (Nothing, input)
+  (res, rest):_ -> (Just res, rest)
+
+parseNumber :: (Num a, Read a) => ReadP (Maybe a)
+parseNumber = readMaybe <$> munch1 isDigit
+
+workspaceNumber :: String -> (Maybe Int, String)
+workspaceNumber = parse workspaceName >>> \case
+  (Just res, _) -> res
+  (Nothing, res) -> (Nothing, res)
+  where workspaceName = do
+          num' <- option Nothing parseNumber
+          label <- parseLabel <++ look
+          eof
+          pure (num', label)
+        colon = char ':'
+        parseLabel = (colon +++ satisfy isSpace)
+          >> skipSpaces
+          >> many get
--- test/Main.hs
+++ test/Main.hs
@@ -1,18 +1,56 @@
 module Main where
 
--- file Spec.hs
 import Test.Hspec
+import I3.Workspaces
 import Test.QuickCheck
-import Control.Exception (evaluate)
+-- import Control.Exception (evaluate)
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+  describe "I3.Workspaces" $ do
+    describe "renumberWorkspaces" $ do
+      it "is unchanged with sequenced numbers" $ do
+        renumberWorkspaces ["1", "2", "3"] `shouldBe` ["1", "2", "3"]
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
+      it "reorders unsorted numbers" $ do
+        renumberWorkspaces ["2", "3", "1"] `shouldBe` ["1", "2", "3"]
 
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+      it "only changes number" $ do
+        renumberWorkspaces ["2: foo", "3: bar", "1: baz"] `shouldBe` ["1: foo", "2: bar", "3: baz"]
+
+      it "adds number to unnumbered workspaces" $ do
+        renumberWorkspaces ["4: foo", "bar", "baz"] `shouldBe` ["1: foo", "2: bar", "3: baz"]
+
+      it "rename whitespace name" $ do
+        renumberWorkspaces [" "] `shouldBe` ["1"]
+
+      it "has idempotence" $ do
+        property $ \xs (Positive n) (Positive m) ->
+          let renames = iterate renumberWorkspaces xs
+          in renames !! n `shouldBe` renames !! m
+
+    describe "workspaceNumber" $ do
+      let noNumString = listOf $ elements (['A'..'Z'] <> ['a'..'z'])
+
+      it "Always no number" $ do
+        property $ forAll noNumString $ \s ->
+          workspaceNumber s `shouldBe` (Nothing, s)
+
+      it "Just number" $ do
+        property $ \(Positive n) -> workspaceNumber (show n) `shouldBe` (Just n, "")
+
+      it "Just whitespace" $ do
+        workspaceNumber "  " `shouldBe` (Nothing, "")
+
+      it "Number and whitespace" $ do
+        property $ \(Positive n) (Positive spaces) ->
+          let s = replicate spaces ' '
+          in workspaceNumber (show n <> s) `shouldBe` (Just n, "")
+
+      it "With number, no colon" $ do
+        property $ forAll noNumString $ \s (Positive n) ->
+          workspaceNumber (show n <> " " <> s) `shouldBe` (Just n, s)
+
+      it "With number and colon" $ do
+        property $ forAll noNumString $ \s (Positive n) ->
+          workspaceNumber (show n <> ": " <> s) `shouldBe` (Just n, s)
