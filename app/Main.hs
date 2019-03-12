54dd4c8be728322287dd49ab05cf6dbc87bbbc92
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-03-12 19:18:52 +0100

    Clean up main a bit

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,8 @@
 module Main where
 
 import I3
-import I3.Workspaces
 
 main :: IO ()
 main = do
   i3 <- initI3
-
-  -- Tree
-  -- print =<< getTree i3
-
-  -- Workspaces
-  -- workspaces <- getWorkspaces i3
-  -- forM_ workspaces print
-  assignNumbers i3
-  -- moveFocusedRight i3
-
-  -- Commands
-  -- command i3 "exec xcalc"
-  -- subscribeEvents i3 [Window] $ \ev -> do
-  --   let (Event _ payload) = ev
-  --   print ev
-  --   case decode payload >>= parseMaybe (.: "change") of
-  --     Nothing     -> putStrLn "No event 'change'"
-  --     Just change -> putStrLn change
+  print =<< getWorkspaces i3
