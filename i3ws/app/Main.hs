aad7952852d28e5942bae6d4759a9a42e0950639
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-03-17 22:47:19 +0100

    i3ws: Make app support workspace moving

--- i3ws/app/Main.hs
+++ i3ws/app/Main.hs
@@ -4,24 +4,17 @@ module Main where
 
 import I3 hiding (command)
 import I3WS
+import I3WS.Workspaces
 import System.Environment
-
-monitor :: IO ()
-monitor = do
-  i3 <- initI3
-  autoRenameWorkspaces i3
-
-data Direction = Left' | Right'
-
-move :: Direction -> IO ()
-move _ = fail "move not implemented"
 
 usage :: IO ()
 usage = putStrLn "i3ws [ monitor | move <dir> ]"
 
 main :: IO ()
-main = getArgs >>= \case
-  ["monitor"]       -> monitor
-  ["move", "left"]  -> move Left'
-  ["move", "right"] -> move Right'
-  _                 -> usage >> fail "Invalid arguments"
+main = do
+  i3 <- initI3
+  getArgs >>= \case
+    ["monitor"]       -> autoRenameWorkspaces i3
+    ["move", "left"]  -> moveLeft i3
+    ["move", "right"] -> moveRight i3
+    _                 -> usage >> fail "Invalid arguments"
