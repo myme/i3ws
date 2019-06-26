20cdccd6827264407936b06195bef316974e07da
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-06-26 23:22:34 +0200

    Add project.yaml (hpack) to findHaskellPackages

--- nix/utils.nix
+++ nix/utils.nix
@@ -8,9 +8,21 @@ let
     findHaskellPackages = root:
         let items = builtins.readDir root;
             fn = file: type:
-                if type == "directory" && isNull (builtins.match "\\..*" file) && !(builtins.elem file ["dist" "dist-new"]) then (findHaskellPackages (root + (/. + file)))
-                else (if type == "regular" then (let m = (builtins.match "(.*)\\.cabal" file); in if !(isNull m) then { "${builtins.elemAt m 0}" = root; } else {})
-                      else {});
+                if type == "directory" && isNull (builtins.match "\\..*" file) && !(builtins.elem file ["dist" "dist-new"])
+                then (findHaskellPackages (root + (/. + file)))
+                else (
+                  if type == "regular"
+                  then (
+                    let
+                      cabal_file = (builtins.match "(.*)\\.cabal" file);
+                      hpack_file = file == "package.yaml";
+                    in
+                      if hpack_file then { "${baseNameOf root}" = root; }
+                      else if !(isNull cabal_file) then { "${builtins.elemAt cabal_file 0}" = root; }
+                      else {}
+                  )
+                  else {}
+                );
         in builtins.foldl' (x: y: x // y) {} (builtins.attrValues (builtins.mapAttrs fn items));
 
 in {
