20b8a07e8ce9fdb6ac572aeda32078c9bb413952
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-07-22 10:06:41 +0200

    nix: Switch to overlays

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,14 +1,12 @@
 let
-  nixpkgs = import <nixpkgs> { inherit config; };
+  nixpkgs = import <nixpkgs> { overlays = [pkgs]; };
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ../.gitignore ];
-  config = {
-    packageOverrides = pkgs: rec {
-      haskellPackages = pkgs.haskell.packages.ghc864.override {
-        overrides = self: super: (
-          builtins.mapAttrs (
-            name: path: super.callCabal2nix name (gitignore path) {}) (import ./packages.nix)
-        );
-      };
+  pkgs = self: super: {
+    haskellPackages = super.haskell.packages.ghc864.override {
+      overrides = self: super: (
+        builtins.mapAttrs (
+          name: path: super.callCabal2nix name (gitignore path) {}) (import ./packages.nix)
+      );
     };
   };
 in
